eafbb72e032f51f378c72c7ee248a0106feb7a40
Diego Marques <dlm352@cornell.edu>
2023-10-29 14:45:09 -0400

    components footer

--- frontend/src/pages/HomePage.tsx
+++ frontend/src/pages/HomePage.tsx
@@ -1,8 +1,13 @@
+import FooterComp from '../components/FooterComp';
+
 export default function HomePage() {
   return (
-    <div>
+    <div className="">
       <h1 className="text-5xl font-semibold px-4 py-2">Hello World!</h1>
       <button className="btn btn-secondary">Example</button>
-    </div >
+
+      {/* This is how you use a component!*/}
+      <FooterComp />
+    </div>
   );
 }
