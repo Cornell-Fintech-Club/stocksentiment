0e859ae3292c47ce65545497da3a2266a472ff21
TushigBili <tushig2003@gmail.com>
2023-11-04 15:10:27 -0400

    Kevin and Zach NavBar Component

--- frontend/src/pages/HomePage.tsx
+++ frontend/src/pages/HomePage.tsx
@@ -1,6 +1,9 @@
+import NavBar from "../components/NavBar";
+
 export default function HomePage() {
   return (
     <div>
+      <NavBar />
       <h1 className="text-5xl font-semibold px-4 py-2">Hello World!</h1>
       <button className="btn btn-secondary">Example</button>
     </div >
