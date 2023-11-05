7ec12ecfeb81a955c8dd6f629201892c262330a3
Diego Marques <dlm352@cornell.edu>
2023-11-05 14:35:25 -0500

    merge

--- frontend/src/pages/HomePage.tsx
+++ frontend/src/pages/HomePage.tsx
@@ -5,10 +5,9 @@ import NavBar from "../components/NavBar";
 export default function HomePage() {
   return (
     <div>
-      <h1 className="text-5xl font-semibold px-4 py-2">CFT Stock Sentiment</h1>
+      <NavBar />
       <SearchBar />
       <Table />
-      <NavBar />
     </div >
   );
 }
