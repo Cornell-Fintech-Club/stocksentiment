03c675dd29314ad42f39d04b8f25e502310fbd82
Chloe Chan <chloeachan0@gmail.com>
2023-11-07 21:08:31 -0500

    Integrate API calls with card carousel
    used api call to curate news article cards on carousel with headline, summary, category, image, and linked to original article

--- frontend/src/components/NewsCard.tsx
+++ frontend/src/components/NewsCard.tsx
@@ -1,19 +1,20 @@
 import React from 'react'
 
-const NewsCard = () => {
+function NewsCard(props: any) {
     return (
         <div className="card card-compact card-bordered w-96 bg-base-100">
-            <figure><img src="https://image.cnbcfm.com/api/v1/image/105569283-1542050972462rts25mct.jpg?v=1542051069&w=740&h=416&ffmt=webp&vtcrop=y" alt="LOL" /></figure>
-            <div className="card-body">
-                <p className="card-title">
-                    Square surges after reporting 64% jump in revenue
-                </p>
-                <p>Shares of Square soared on Tuesday evening after posting better-than-expected quarterly results and strong growth in its consumer payments app.</p>
-                <div className="card-actions justify-end">
-                    <div className="badge badge-outline">Technology</div>
-                    <div className="badge badge-outline">Products</div>
+            <a href={props.url}>
+                <figure><img src={props.image} alt="LOL" /></figure>
+                <div className="card-body">
+                    <p className="card-title">{props.headline}
+                    </p>
+                    <p>{props.summary}</p>
+                    <div className="card-actions justify-end">
+                        <div className="badge badge-outline">{props.category}</div>
+                        {/* <div className="badge badge-outline">Products</div> */}
+                    </div>
                 </div>
-            </div>
+            </a>
         </div>
     )
 }
--- frontend/src/components/StockNews.tsx
+++ frontend/src/components/StockNews.tsx
@@ -1,21 +1,40 @@
-import React from 'react';
+import axios from 'axios';
+import React, { useEffect, useState } from 'react';
 import NewsCard from './NewsCard';
 
 const StockNews = () => {
-    return (
-        <div className="carousel carousel-center rounded-box w-6/12">
-            <div className="carousel-item">
-                <NewsCard />
-            </div>
-            <div className="carousel-item">
-                <NewsCard />
-            </div>
-            <div className="carousel-item">
-                <NewsCard />
-            </div>
-        </div>
+    const [data, setData] = useState([]);
+    useEffect(() => {
+        const fetchData = async () => {
+            const token = 'cl3v7d1r01qj63a9lv80cl3v7d1r01qj63a9lv8g'
+            const res = await axios({
+                method: 'get',
+                url: ('https://finnhub.io/api/v1/news?category=general' + '&token=' + token),
+            });
+            setData(res.data)
+        };
 
+        fetchData();
+
+        const interval = setInterval(fetchData, 60000); // Call the API every 60 seconds
+
+        return () => clearInterval(interval);
+    }, []);
+    return (
+        <div className="carousel carousel-center rounded-box w-6/12">{createNewsCards(data)}</div >
     )
+}
+function createNewsCards(data: string | any[]) {
+    const res_news_cards = []
+    for (var i = 0; i < data.length; i++) {
+        var news_article = data[i];
+        // <p>{news_article.category} {news_article.datetime}</p
+        res_news_cards.push(<div className="carousel-item"><NewsCard category={news_article.category}
+            date={news_article.datetime} image={news_article.image} summary={news_article.summary} headline={news_article.headline}
+            source={news_article.source} url={news_article.url}
+        /> </div>);
+    }
+    return res_news_cards;
 }
 
 export default StockNews
