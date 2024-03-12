9ad12a1a8323f1b425e90f68616bdac13cf0ff06
Kevin Biliguun <ktb53@cornell.edu>
2024-03-11 21:24:18 -0400

    Added firebase

--- frontend/src/pages/HomePage.tsx
+++ frontend/src/pages/HomePage.tsx
@@ -1,9 +1,27 @@
-import NavBar from "../components/NavBar";
+import { useEffect } from "react";
 import SearchBar from "../components/SearchBar";
 import Table from "../components/table";
 import TickerInput from "../components/TickerInput";
+import { firestore } from '../firebase'
+import { collection, getDocs } from '@firebase/firestore'
 
 export default function HomePage() {
+  useEffect(() => {
+    const fetchUsers = async () => {
+      try {
+        const usersCollection = collection(firestore, "users");
+        const usersSnapshot = await getDocs(usersCollection);
+        usersSnapshot.forEach((doc) => {
+          console.log(doc.id, " => ", doc.data());
+        });
+      } catch (error) {
+        console.error("Error fetching users: ", error);
+      }
+    };
+
+    fetchUsers();
+  }, []);
+
   return (
     <div>
       {/*<SearchBar />*/}
